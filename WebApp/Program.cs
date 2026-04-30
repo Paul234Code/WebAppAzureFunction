81720dcc6be30d62af38973f591766270778fc37
Paul Faye <fayepaul234@gmail.com>
2026-04-29 21:30:05 -0400

    Mettre a jour le code

--- WebApp/Program.cs
+++ WebApp/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.OpenApi;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,7 +8,34 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "WebAppAzureFunction",
+        Version = "v1",
+        Description = "Web App Azure Function ASP.NET Core Web API with Swagger",
+        Contact = new OpenApiContact
+        {
+            Name = "Paul Faye",
+            Email = "fayepaul234@gmail.com"
+        }
+    });
+});
+
 var app = builder.Build();
+
+// Enable Swagger in Development (or all environments if you prefer)
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebAppAzureFunction v1");
+        c.RoutePrefix = string.Empty; // Swagger at root URL
+    });
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
